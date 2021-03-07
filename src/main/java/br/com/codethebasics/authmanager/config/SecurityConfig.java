ffe58d52fc6ebb142be2da08594e5d93dfd61c04
Bruno Carneiro <bruno.ferreira@trf1.jus.br>
2021-03-07 18:54:35 -0300

    adicionando perfis do maven e arquivos de propriedade

---
+++ src/main/java/br/com/codethebasics/authmanager/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package br.com.codethebasics.authmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
